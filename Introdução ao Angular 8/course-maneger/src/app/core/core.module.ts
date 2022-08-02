d51489fdd57040c1ae56ba3608777c7e48bb7d48
Kaique Santos <skaique5424@gmail.com>
2022-08-01 23:00:38 -0300

    finalizado o curso instalando e preparando o ambiente angular

---
+++ Introdução ao Angular 8/course-maneger/src/app/core/core.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { Error404component } from "./component/error-404/error-404.component";
+import { NavBarComponent } from "./component/nav-bar/nav-bar.component";
+
+@NgModule({
+    declarations: [
+        NavBarComponent,
+        Error404component  
+    ],
+    imports: [
+        RouterModule.forChild([
+            {
+                path: '**', component: Error404component
+              }
+        ])
+    ],
+    exports:[
+        NavBarComponent
+    ]
+})
+export class CoreModule{
+
+}
